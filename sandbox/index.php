2bd24a9b4a398f3683aaac2e8853f6589613bd9c
Christian Achatz <christian.achatz@adventure-php-framework.org>
2011-02-08 20:26:05 +0000

    Small adaptions to the sandbox.

--- sandbox/index.php
+++ sandbox/index.php
@@ -1,22 +1,20 @@
 <?php
-   // initialize language if language was sent by the browser
-   $lang = 'en';
-   if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-      if (substr_count($_SERVER['HTTP_ACCEPT_LANGUAGE'], 'de') > 0) {
-         $lang = 'de';
-      }
-   }
+// initialize language if language was sent by the browser
+$lang = 'en';
+if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) && substr_count($_SERVER['HTTP_ACCEPT_LANGUAGE'], 'de') > 0) {
+   $lang = 'de';
+}
 
-   // configure PHP's error message type
-   ini_set('html_errors', 'off');
+// configure PHP's error message type
+ini_set('html_errors', 'off');
 
-   // include the pagecontroller
-   include_once('./apps/core/pagecontroller/pagecontroller.php');
+// include the pagecontroller
+include_once('./apps/core/pagecontroller/pagecontroller.php');
 
-   // create the sandbox page
-   $page = new Page();
-   $page->setContext('myapp');
-   $page->setLanguage($lang);
-   $page->loadDesign('sandbox::pres::templates', 'main');
-   echo $page->transform();
+// create the sandbox page
+$page = new Page();
+$page->setContext('myapp');
+$page->setLanguage($lang);
+$page->loadDesign('sandbox::pres::templates', 'main');
+echo $page->transform();
 ?>
