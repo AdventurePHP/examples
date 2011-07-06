fa4e894e5c46d297f52c7068199d221142699858
Christian Achatz <christian.achatz@adventure-php-framework.org>
2011-07-06 05:29:33 +0000

    Added listener:addtaglib tag as desired under http://forum.adventure-php-framework.org/de/viewtopic.php?f=3&t=765.

--- sandbox/apps/sandbox/pres/taglib/curr_taglib_date.php
+++ sandbox/apps/sandbox/pres/taglib/curr_taglib_date.php
@@ -1,34 +1,28 @@
 <?php
+/**
+ * @namespace sites::apfexample::pres::taglib
+ *
+ *  TagLib implementation to display the current date and time.<br />
+ *
+ * @author Christian Achatz
+ * @version
+ *  Version 0.1, 20.04.2008<br />
+ */
+class curr_taglib_date extends Document {
+
    /**
-   *  @namespace sites::apfexample::pres::taglib
-   *
-   *  TagLib implementation to display the current date and time.<br />
-   *
-   *  @author Christian Achatz
-   *  @version
-   *  Version 0.1, 20.04.2008<br />
-   */
-   class curr_taglib_date extends Document
-   {
-
-      function curr_taglib_date(){
-      }
-
-
-      /**
-      *  @public
-      *
-      *  Implements the Document's transform() method. Returns the current date and time.<br />
-      *
-      *  @author Christian Achatz
-      *  @version
-      *  Version 0.1, 20.04.2008<br />
-      */
-      function transform(){
-         return date('l, d.m.Y, H:i:s');
-       // end function
-      }
-
-    // end class
+    * @public
+    *
+    *  Implements the Document's transform() method. Returns the current date and time.<br />
+    *
+    * @author Christian Achatz
+    * @version
+    *  Version 0.1, 20.04.2008<br />
+    */
+   public function transform() {
+      return date('l, d.m.Y, H:i:s');
    }
+
+}
+
 ?>
