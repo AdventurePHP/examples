36682a940bbfcc3c02f556486ed9083f79537b62
Christian Achatz <christian.achatz@adventure-php-framework.org>
2011-09-16 21:05:37 +0000

    Finished implementation of the registration form.

--- sandbox/index.php
+++ sandbox/index.php
@@ -1,5 +1,5 @@
 <?php
-// initialize language if language was sent by the browser
+// initialize language if is is sent by the browser
 $lang = 'en';
 if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) && substr_count($_SERVER['HTTP_ACCEPT_LANGUAGE'], 'de') > 0) {
    $lang = 'de';
