47042aa3cc60b8253856243a54dec7ecb73e04fd
Christian Achatz <christian.achatz@adventure-php-framework.org>
2012-01-14 15:25:09 +0000

    * First implementation of the localization of the user management front-end. * Removed the <core:addtaglib /> statements for <core:appendnode />. * Updated the umgt.css.

---
+++ sandbox/apps/sandbox/pres/taglib/lang_taglib_umgtimport.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package sandbox::pres::taglib
+ * @class lang_taglib_umgtimport
+ *
+ * Implements a special tag lib to import language templates from
+ * a prefixed namespace.
+ */
+class lang_taglib_umgtimport extends core_taglib_importdesign {
+
+   public function onParseTime() {
+      // re-maps the namespace to the language dependent sub-path
+      // to be able to save the content within language dependent
+      // templates under the "normal" namespace.
+      $this->setAttribute(
+         'namespace',
+            $this->getAttribute('namespace') . '::' . $this->getLanguage()
+      );
+      parent::onParseTime();
+   }
+
+}
+
+?>
