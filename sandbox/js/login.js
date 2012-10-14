9e26cde58bf8216aea0c48620c7dc6c5f63c44df
Christian Achatz <christian.achatz@adventure-php-framework.org>
2012-10-14 21:09:17 +0000

    Integrated permanent login by Coach83 incl. configuration files and sandbox integration.

--- sandbox/js/login.js
+++ sandbox/js/login.js
@@ -1,30 +1,42 @@
 /**
- * Initializes the login form with prefilled values in case the labels are not displayed.
+ * Initializes the login form with pre-filled values in case the labels are not displayed.
  */
 function init(fieldSelector, defaultValue, bgColor, fgColor) {
-   var currentValue = $('.umgt-fe-login ' + fieldSelector).attr('value');
+    var currentValue = $('.umgt-fe-login ' + fieldSelector).attr('value');
 
-   if(currentValue.length == 0) {
-      $('.umgt-fe-login ' + fieldSelector)
-      .attr('value', defaultValue)
-      .css('color', bgColor);
-   }
+    if (currentValue.length == 0) {
+        $('.umgt-fe-login ' + fieldSelector)
+            .attr('value', defaultValue)
+            .css('color', bgColor);
+    }
 
-   $('.umgt-fe-login ' + fieldSelector)
-   .focus(function(){
-      if($(this).attr('value') == defaultValue){
-         $(this).attr('value', '').css('color', fgColor);
-      }
-   })
-   .blur(function(){
-      if($(this).attr('value').length < 1){
-         $(this).attr('value', defaultValue).css('color', bgColor);
-      }
-   });
-   $('.umgt-fe-login').submit(function(){
-      var field = $(this).find(fieldSelector);
-      if(field.attr('value') == defaultValue){
-         field.attr('value', '');
-      }
-   });
+    $('.umgt-fe-login ' + fieldSelector)
+        .focus(function () {
+            if ($(this).attr('value') == defaultValue) {
+                $(this).attr('value', '').css('color', fgColor);
+            }
+        })
+        .blur(function () {
+            if ($(this).attr('value').length < 1) {
+                $(this).attr('value', defaultValue).css('color', bgColor);
+            }
+        });
+    $('.umgt-fe-login').submit(function () {
+        var field = $(this).find(fieldSelector);
+        if (field.attr('value') == defaultValue) {
+            field.attr('value', '');
+        }
+    });
+}
+
+/**
+ * Transfers the label of the remember-me field to the field's title to keep the log-in form simple.
+ */
+function initStayLoggedInTitle(labelFieldSelector) {
+    $('#umgt-fe-login-remember-me').attr(
+        'title',
+        $.trim(
+            $('.umgt-fe-login label[for="' + labelFieldSelector + '"]').text()
+        )
+    );
 }
