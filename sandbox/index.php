e8cf28864edc6e96ed0bc1c00aa5f09961b477a6
Christian Achatz <christian.achatz@adventure-php-framework.org>
2013-03-14 22:25:03 +0000

    Added logger configuration to sandbox bootstrap file.

--- sandbox/index.php
+++ sandbox/index.php
@@ -1,6 +1,4 @@
 <?php
-define('APPS__PATH', 'D:/Apache2.2/htdocs/www/sandbox/apps');
-
 // initialize language if is is sent by the browser
 $lang = 'en';
 if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) && substr_count($_SERVER['HTTP_ACCEPT_LANGUAGE'], 'de') > 0) {
@@ -13,9 +11,24 @@ ini_set('html_errors', 'off');
 // include the pagecontroller
 include_once('./apps/core/pagecontroller/pagecontroller.php');
 
+// prepare logger for different database drivers
+import('core::logging', 'Logger');
+/* @var $l Logger */
+$l = & Singleton::getInstance('Logger');
+$stdWriter = clone $l->getLogWriter(
+   Registry::retrieve('apf::core', 'InternalLogTarget')
+);
+$l->addLogWriter('mysqlx', clone $stdWriter);
+$l->addLogWriter('mysqli', clone $stdWriter);
+$l->addLogWriter('pdo', clone $stdWriter);
+$l->addLogWriter('sqlite', clone $stdWriter);
+
+// write all log entries to file to allow easy debugging
+$l->setLogThreshold(Logger::$LOGGER_THRESHOLD_ALL);
+
 // create the sandbox page
 import('core::frontcontroller', 'Frontcontroller');
-$fC = &Singleton::getInstance('Frontcontroller');
+$fC = & Singleton::getInstance('Frontcontroller');
 /* @var $fC Frontcontroller */
 $fC->setContext('myapp');
 $fC->setLanguage($lang);
