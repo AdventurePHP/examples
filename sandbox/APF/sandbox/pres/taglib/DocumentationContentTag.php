3827e08e142f534537e117d097d040819bd9c466
Christian Achatz <christian.achatz@adventure-php-framework.org>
2013-04-05 20:58:07 +0000

    * Further code corrections. * Removed explicit namespace declaration using PHP classes. * Updated sandbox sample configuration files. * Reformatted code for better readability. * Migrated namespace notation from :: to \ separation. * Corrected service manager calls.

--- sandbox/APF/sandbox/pres/taglib/DocumentationContentTag.php
+++ sandbox/APF/sandbox/pres/taglib/DocumentationContentTag.php
@@ -28,7 +28,7 @@ class DocumentationContentTag extends Document {
       $content = preg_replace('/<int:link pageid="([0-9]+)"(.+)?>(.+)<\/int:link>/iU', '<a href="http://adventure-php-framework.org/Seite/119-Komponenten-Dokumentation">[' . $onlineDocAnchorName . ']</a>', $content);
       $content = preg_replace('/<int:link pageid="([0-9]+)"(.+)?\/>/iU', '<a href="http://adventure-php-framework.org/Seite/119-Komponenten-Dokumentation">[' . $onlineDocAnchorName . ']</a>', $content);
       $content = preg_replace('/<doku:link>(.+)<\/doku:link>/isU', '<a href="$1">$1</a>', $content);
-      $content = preg_replace('/<core:importdesign namespace="modules::comments::pres::templates" template="comment" categorykey="(.+)" \/>/isU', '', $content);
+      $content = preg_replace('/<core:importdesign namespace="APF\modules\comments\pres\templates" template="comment" categorykey="(.+)" \/>/isU', '', $content);
       $content = preg_replace('/<gen:highlight type="([a-z\-]+)">(.+)<\/gen:highlight>/isU', '<pre>$2</pre>', $content);
       return preg_replace('/<doku:title (.+) title="(.+)" (.+)>(.+)<\/doku:title>/isU', '<h2>$2</h2>', $content);
    }
