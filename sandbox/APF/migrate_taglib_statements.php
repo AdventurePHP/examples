bd69e684ab9dc77fc6042d49f2be5128671f5175
Christian Achatz <christian.achatz@adventure-php-framework.org>
2013-04-06 10:15:54 +0000

    Migrated <*:getstring /> tag calls.

--- sandbox/APF/migrate_taglib_statements.php
+++ sandbox/APF/migrate_taglib_statements.php
@@ -3,9 +3,11 @@ include(dirname(__FILE__) . '/migrate_base.php');
 
 $files = find('.', '*.html');
 
-$searchAddTaglib = '#<([A-Za-z0-9\-]+):addtaglib([\* |\n|\r\n]+)namespace ?= ?"([A-Za-z0-9:\-]+)"([\* |\n|\r\n]+)class ?= ?"([A-Za-z0-9\-]+)"#';
+$searchAddTaglib = '#<([A-Za-z0-9\-]+):addtaglib([\*| |\n|\r\n]+)namespace ?= ?"([A-Za-z0-9:\-]+)"([\*| |\n|\r\n]+)class ?= ?"([A-Za-z0-9\-]+)"#';
 
-$searchSingleNamespace = '#<([A-Za-z0-9\-]+):importdesign([\* |\n|\r\n]+)namespace="([A-Za-z0-9:\-]+)"#';
+$searchSingleNamespace = '#<([A-Za-z0-9\-]+):importdesign([\*| |\n|\r\n]+)namespace="([A-Za-z0-9:\-]+)"#';
+
+$searchGetStringNamespace = '#<([A-Za-z0-9\-]+):getstring([\*| |\n|\r\n]+)namespace="([A-Za-z0-9:\-]+)"#';
 
 foreach ($files as $file) {
    $content = file_get_contents($file);
@@ -18,6 +20,11 @@ foreach ($files as $file) {
    // replace <*:importdesign /> calls
    $content = preg_replace_callback($searchSingleNamespace, function ($matches) {
       return '<' . $matches[1] . ':importdesign' . $matches[2] . 'namespace="APF\\' . str_replace('::', '\\', $matches[3]) . '"';
+   }, $content);
+
+   // replace <*:getstring /> calls
+   $content = preg_replace_callback($searchGetStringNamespace, function ($matches) {
+      return '<' . $matches[1] . ':getstring' . $matches[2] . 'namespace="APF\\' . str_replace('::', '\\', $matches[3]) . '"';
    }, $content);
 
    file_put_contents($file, $content);
